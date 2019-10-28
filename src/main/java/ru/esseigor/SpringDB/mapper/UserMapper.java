8290e6c8e6828194efc8361812b3de3f4153ac52
esseigor <esseigor@gmail.com>
2019-10-28 20:46:38 +0400

    Создание класса Mapper

---
+++ src/main/java/ru/esseigor/SpringDB/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package ru.esseigor.SpringDB.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.esseigor.SpringDB.model.UserInfo;
+
+public class UserMapper implements RowMapper<UserInfo>{
+
+	public static final String BASE_SQL = "SELECT u.id id, u.login login, u.fio fio FROM db_app.users u ";
+	
+	@Override
+	public UserInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		Long id = rs.getLong("id");
+		String login = rs.getString("login");
+		String fio = rs.getString("fio");
+		
+		return new UserInfo(id, login, fio);
+	}
+
+}
